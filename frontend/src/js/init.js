186a9ba4cb5b9804f5243114f355e510ff36405f
Monica <mmarort302@g.educaand.es>
2025-05-05 13:22:03 +0200

    solucionado error en init

--- frontend/src/js/init.js
+++ frontend/src/js/init.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Importa Router, Routes y Route
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "../css/init.css";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
@@ -10,15 +10,11 @@ import RegisterForm from "./register/RegisterForm";
 function Init() {
   return (
     <Router>
-      {" "}
-      {/* Envolvemos toda la app en Router */}
       <div className="App">
         <Banner />
         <Navbar />
-<<<<<<< HEAD
+
         <Routes>
-          {" "}
-          {/* Definimos las rutas */}
           <Route
             path="/"
             element={
@@ -30,22 +26,6 @@ function Init() {
           />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/registro" element={<RegisterForm />} />
-          <Route path="/" element={<Init />} />{" "}
-          {/* Ruta para el formulario de inicio */}
-          <Route path="/login" element={<LoginForm />} />{" "}
-          {/* Ruta para el formulario de login */}
-=======
-        <Routes> {/* Definimos las rutas */}
-          <Route path="/" element={<div className="movie-grid">
-            <MovieCard titulo="Título" descripcion="Descripción" />
-            <MovieCard titulo="Título" descripcion="Descripción" />
-          </div>} />
-
-          <Route path="/registro" element={<RegisterForm />} />
-          <Route path="/" element={<Init />} /> {/* Ruta para el formulario de inicio */}
-          <Route path="/login" element={<LoginForm />} /> {/* Ruta para el formulario de login */}
-
->>>>>>> origin/carmen
         </Routes>
       </div>
     </Router>
