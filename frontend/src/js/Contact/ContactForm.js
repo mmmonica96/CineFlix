42c52e0bff2b2a6efd0e6a4cc0dd2048bf43a3e5
CarmenGil05 <gilvazquezcarmendelrocio@gmail.com>
2025-05-06 17:23:20 +0200

    arreglando conflictos2.0

--- frontend/src/js/Contact/ContactForm.js
+++ frontend/src/js/Contact/ContactForm.js
@@ -2,10 +2,6 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../../css/Contact.css';
 
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import "../../css/Contact.css";
-
 
 function ContactForm() {
   const [name, setName] = useState("");
